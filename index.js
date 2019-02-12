673e46d8fe7bcd0ab0c064edd4eab3effbc32b95
Luis Hernandez <luishrd@gmail.com>
2019-02-12 09:59:10 -0700

    updates instructions

--- index.js
+++ index.js
@@ -92,6 +92,7 @@ server.put('/api/hubs/:id', async (req, res) => {
 });
 
 // add an endpoint that returns all the messages for a hub
+// add an endpoint for adding new message to a hub
 
 server.listen(4000, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
