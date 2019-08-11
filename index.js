88c4b4adc98932210f3b3c6231ef75787d6df063
Diandra Ryan-Mas <dryanmas@Diandras-MacBook-Air.local>
2019-08-11 09:05:16 -0700

    refactored to promises

--- index.js
+++ index.js
@@ -13,82 +13,87 @@ server.get('/', (req, res) => {
   `);
 });
 
-server.get('/api/hubs', async (req, res) => {
-  try {
-    const hubs = await Hubs.find(req.query);
+server.get('/api/hubs', (req, res) => {
+  Hubs.find(req.query)
+  .then(hubs => {
     res.status(200).json(hubs);
-  } catch (error) {
+  })
+  .catch(error => {
     // log error to database
     console.log(error);
     res.status(500).json({
       message: 'Error retrieving the hubs',
     });
-  }
+  });
 });
 
-server.get('/api/hubs/:id', async (req, res) => {
-  try {
-    const hub = await Hubs.findById(req.params.id);
-
+server.get('/api/hubs/:id', (req, res) => {
+  Hubs.findById(req.params.id)
+  .then(hub => {
     if (hub) {
       res.status(200).json(hub);
     } else {
       res.status(404).json({ message: 'Hub not found' });
     }
-  } catch (error) {
+  })
+  .catch(error => {
     // log error to database
     console.log(error);
     res.status(500).json({
       message: 'Error retrieving the hub',
     });
-  }
+  });
 });
 
-server.post('/api/hubs', async (req, res) => {
-  try {
-    const hub = await Hubs.add(req.body);
+server.post('/api/hubs', (req, res) => {
+  Hubs.add(req.body)
+  .then(hub => {
     res.status(201).json(hub);
-  } catch (error) {
+  })
+  .catch(error => {
     // log error to database
     console.log(error);
     res.status(500).json({
       message: 'Error adding the hub',
     });
-  }
+  });
 });
 
-server.delete('/api/hubs/:id', async (req, res) => {
-  try {
-    const count = await Hubs.remove(req.params.id);
+server.delete('/api/hubs/:id', (req, res) => {
+  Hubs.remove(req.params.id)
+  .then(count => {
     if (count > 0) {
       res.status(200).json({ message: 'The hub has been nuked' });
     } else {
       res.status(404).json({ message: 'The hub could not be found' });
     }
-  } catch (error) {
+  })
+  .catch(error => {
     // log error to database
     console.log(error);
     res.status(500).json({
       message: 'Error removing the hub',
     });
-  }
+  });
 });
 
-server.put('/api/hubs/:id', async (req, res) => {
-  try {
-    const hub = await Hubs.update(req.params.id, req.body);
+server.put('/api/hubs/:id', (req, res) => {
+  const changes = req.body;
+  Hubs.update(req.params.id, changes)
+  .then(hub => {
     if (hub) {
       res.status(200).json(hub);
     } else {
       res.status(404).json({ message: 'The hub could not be found' });
     }
-  } catch (error) {
+  })
+  .catch(error => {
     // log error to database
     console.log(error);
     res.status(500).json({
       message: 'Error updating the hub',
     });
-  }
+  });
 });
 
 // add an endpoint that returns all the messages for a hub
