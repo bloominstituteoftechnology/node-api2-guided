b5efb85e5c042d0ea931761cc3a95b2b58e3760b
Ladrillo <gabriel@cabrejas.net>
2022-01-11 11:50:58 +0100

    update deps, adopters

--- data/seeds/001-adopters.js
+++ data/seeds/001-adopters.js
@@ -1,9 +1,9 @@
 exports.seed = function(knex) {
   return knex('adopters').insert([
-    { name: 'Katherine', email: 'kat@bloomtech.com' },
-    { name: 'Felipe', email: 'felipe@bloomtech.com' },
-    { name: 'Wailon', email: 'wailon@bloomtech.com' },
-    { name: 'Raj', email: 'raj@bloomtech.com' },
-    { name: 'Priscila', email: 'priscila@bloomtech.com' },
+    { name: 'Roberto', email: 'roberto@bloomtech.com' },
+    { name: 'Anastasia', email: 'anastasia@bloomtech.com' },
+    { name: 'Areccus', email: 'areccus@bloomtech.com' },
+    { name: 'Danielle', email: 'danielle@bloomtech.com' },
+    { name: 'William', email: 'william@bloomtech.com' },
   ]);
 }
